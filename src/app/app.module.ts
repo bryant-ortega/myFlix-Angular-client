164b523e85cec1bc07389764997afcece0d425f7
Bryant Ortega <kalicorose@gmail.com>
2023-08-27 16:17:22 -0700

    installed Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+     
 
 @NgModule({
   declarations: [
@@ -12,7 +22,15 @@ import { HttpClientModule } from '@angular/common/http'
   imports: [
     BrowserModule,
     HttpClientModule,    
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    BrowserAnimationsModule, 
+    MatDialogModule, 
+    MatInputModule,
+    MatButtonModule, 
+    MatCardModule,
+    MatFormFieldModule, 
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
